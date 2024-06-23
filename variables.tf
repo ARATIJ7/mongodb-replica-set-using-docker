402b1b7728f0a301fb99ec9f377e80e231ec23b9
Arati Jagtap <154422939+ARATIJ7@users.noreply.github.com>
2024-06-23 19:59:11 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "The AWS region to create resources in"
-  default     = "us-east-1"
+  default     = "ap-southeast-2"
 }
 
 variable "instance_type" {
   description = "The type of instance to create"
-  default     = "t3.medium"
+  default     = "t2.micro"
 }
 
 variable "key_name" {
